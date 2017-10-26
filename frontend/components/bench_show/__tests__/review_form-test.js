bd08d43d5b618bab1ea355b18ac255aa763d0930
sohbr <sohnbrian@gmail.com>
2017-10-26 15:47:37 -0700

    write filterform test

--- frontend/components/bench_show/__tests__/review_form-test.js
+++ frontend/components/bench_show/__tests__/review_form-test.js
@@ -1,54 +1,55 @@
-import React from 'react';
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
-import { mount } from 'enzyme';
-import ReviewFormContainer from '../review_form_container';
-import MockRouter from 'react-mock-router';
+import React from "react";
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
+import { mount } from "enzyme";
+import ReviewFormContainer from "../review_form_container";
+import MockRouter from "react-mock-router";
 
 const bench = { id: 1, description: "great!", lat: 1.2, lng: 3.4 };
-const middlewares = [ thunk ];
+const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const testStore = mockStore({ benches: { [bench.id]: bench } });
 const push = jest.fn();
 
-describe('review form container', () => {
-  let reviewFormWrapper,
-      reviewRating,
-      reviewBody,
-      benchShowButton;
+describe("review form container", () => {
+  let reviewFormWrapper, reviewRating, reviewBody, benchShowButton;
 
-  describe('creating a new review', () => {
+  describe("creating a new review", () => {
     beforeEach(() => {
       reviewFormWrapper = mount(
         <MockRouter push={push}>
           <ReviewFormContainer store={testStore} />
         </MockRouter>
-      ).find('ReviewForm');
+      ).find("ReviewForm");
 
-      reviewRating = reviewFormWrapper.find('input').filterWhere(input => (
-        input.props().type === 'number'
-      ));
-      reviewBody = reviewFormWrapper.find('textarea');
-      benchShowButton = reviewFormWrapper.find('button');
+      reviewRating = reviewFormWrapper
+        .find("input")
+        .filterWhere(input => input.props().type === "number");
+      reviewBody = reviewFormWrapper.find("textarea");
+      benchShowButton = reviewFormWrapper.find("button");
       console.log(reviewFormWrapper.debug());
     });
 
-    test('correctly maps dispatch to props', () => {
+    test("correctly maps dispatch to props", () => {
       // Your code here
+      expect(reviewFormWrapper.props().createReview).toBeDefined();
     });
 
-    test('pre-fills rating field to 5', () => {
+    test("pre-fills rating field to 5", () => {
       // Your code here
+      expect(reviewRating.props().value).toEqual(5);
     });
 
-    test('pre-fills comment field with empty string', () => {
+    test("pre-fills comment field with empty string", () => {
       // Your code here
+      expect(reviewBody.props().value).toEqual("");
     });
 
-    test('navigates to bench show on button click', () => {
+    test("navigates to bench show on button click", () => {
       // Refer to react-mock-router docs
-     // Your code here
+      // Your code here
+      benchShowButton.simulate("click");
+      expect(push).toBeCalledWith("/benches/undefined");
     });
   });
-
 });
